679b51aa8aaa445c798ebec30619615119447bcb
zertalls <zertalls@mail.ru>
2025-11-28 23:11:47 +0300

    TaskFive completed

--- src/TaskFive/TaskFive.tsx
+++ src/TaskFive/TaskFive.tsx
@@ -5,38 +5,36 @@ type StepType = {
     durationMs: number
 }
 
+const steps: StepType[] = [
+    {name: 'Разогрев', durationMs: 1000},
+    {name: 'Приготовление', durationMs: 1000},
+    {name: 'Подача', durationMs: 1000},
+]
+
 export const TaskFive = () => {
-    const [stepIndex, setStepIndex] = useState(-1)
-    // const [timeoutId, setTimeoutId] = useState<number | undefined>(undefined)
-
-    const steps: StepType[] = [
-        {name: 'Разогрев', durationMs: 5000},
-        {name: 'Приготовление', durationMs: 10000},
-        {name: 'Подача', durationMs: 3000},
-    ]
-
-    useEffect(() => {
+    const [stepIndex, setStepIndex] = useState(0)
+    
+    useEffect(() => {  
+        if (stepIndex === steps.length) {
+            return
+        }
+            
         const timeoutId = setTimeout(() => {
-            setStepIndex(prev =>{
-                if (prev === steps.length) {                    
-                    return 2
-                } else {
-                    return prev + 1
-                }
-            })
-        }, steps[stepIndex].durationMs)  
-        
+            setStepIndex(prev => prev + 1)
+        }, steps[stepIndex].durationMs)          
+
         return () => {
             clearTimeout(timeoutId)
         }           
     }, [stepIndex])
 
     return (
-        <div>           
-            <span>{steps[stepIndex].name}</span>
+        <div>
+            { stepIndex === steps.length ? <span>ФИНИШЬ!</span> : <span>{steps[stepIndex].name}</span>} 
         </div>
     );
 };
+
 
 //? Задача 5: Таймер приготовления
 //? Цель: Таймер для готовки с несколькими этапами.
@@ -45,10 +43,10 @@ export const TaskFive = () => {
 
 //TODO: Автоматический переход между этапами
 
-//TODO: Отображение текущего этапа и прогресс-бар
-
 //* Особенности:
 
 //* Как организовать цепочку таймеров?
 
 //* Как сбрасывать всю последовательность?
+
+//TODO: Отображение текущего этапа и прогресс-бар
